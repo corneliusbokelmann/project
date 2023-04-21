9fb0ab1c04ae98c778c0a5a4b32283215bbd2ee4
DanielPaustian <paustian.daniel@gmail.com>
2023-04-21 09:13:54 +0200

    Added second field +String representation to match

--- game/src/main/scala/de/htwg/se/model/Field.scala
+++ game/src/main/scala/de/htwg/se/model/Field.scala
@@ -5,17 +5,23 @@ case class Field(matrix: Matrix[Point]){
   def this(pointslength: Int, guesslegth: Int, filling: Point) = this(new Matrix(pointslength, guesslegth, filling))
   val eol = sys.props("line.separator")
 
-  def bar(barWidth: Int = matrix.pointslength): String = "+---" * barWidth + "+" + eol
+  def bar(barWidth: Int = matrix.pointslength / 2): String =
+    val s = ("+---" * barWidth + "+")
+    val x = (s + " " + s + eol)
+    x.toString()
+
+  //def vec(row: Int): IndexedSeq[Point] = (0 until matrix.pointslength / 2).map(matrix.rows(row))
+
   def cells(row : Int): String = 
-    matrix.row(row).map(_.toString()).map(" " + _ + " " ).mkString("|","|","|") + eol
-  def mesh(cellWidth: Int = matrix.pointslength): String =
+    //val first = vec(row).map(_.toString()).map(" " + _ + " " ).mkString("|","|","|")
+    val first = (0 until (matrix.pointslength - 1) / 2).map(matrix.rows(row)).map(_.toString()).map(" " + _ + " " ).mkString("|","|","|")
+    val second = ((matrix.pointslength - 1) / 2 until matrix.pointslength - 1).map(matrix.rows(row)).map(_.toString()).map(" " + _ + " " ).mkString("|","|","|")
+    val x = first + " " + second + eol
+    x.toString()
+
+  def mesh(cellWidth: Int = matrix.pointslength / 2): String =
     (0 until matrix.guesslegth).map(cells(_)).mkString(bar(cellWidth), bar(cellWidth), bar(cellWidth))
 
-  // def bar(cellWidth: Int, cellNum: Int): String = (("+" + "-" * cellWidth) * cellNum) + "+" + eol
-  // def cells(row : Int, cellWidth: Int) = 
-  //   matrix.row(row).map(_.toString()).map(" " * ((cellWidth - 1) / 2) + _ + " " * ((cellWidth - 1) / 2)).mkString("|","|","|") + eol
-  // def mesh(cellWidth: Int = 5): String =
-  //   (0 until matrix.guesslegth).map(cells(_,cellWidth)).mkString(bar(cellWidth, matrix.pointslength), bar(cellWidth, matrix.pointslength), bar(cellWidth, matrix.pointslength))
   override def toString = mesh()
   def put(point: Point, x: Int, y: Int) = copy(matrix.replaceCell(x, y, point))
 }
--- game/src/main/scala/de/htwg/se/model/Matrix.scala
+++ game/src/main/scala/de/htwg/se/model/Matrix.scala
@@ -2,7 +2,7 @@
 package de.htwg.se.model 
 
 case class Matrix[T](rows: Vector[Vector[T]]) {
-  def this(pointslength: Int, guesslegth: Int, filling: T) = this(Vector.tabulate(guesslegth, pointslength) {(row, col) => filling})
+  def this(pointslength: Int, guesslegth: Int, filling: T) = this(Vector.tabulate(guesslegth, pointslength * 2 + 1) {(row, col) => filling})
   val guesslegth: Int = rows.length
   val pointslength: Int = rows(0).length 
   def cell(row: Int, col: Int): T = rows(row)(col)
