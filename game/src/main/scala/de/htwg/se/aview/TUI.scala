4663e4eb45eef1b38a4d263a1a6a2bcdd47ab12f
corneliusbokelmann <bokelmanncornelius@web.de>
2023-04-27 13:27:52 +0200

    fixed bug with tui

--- game/src/main/scala/de/htwg/se/aview/TUI.scala
+++ game/src/main/scala/de/htwg/se/aview/TUI.scala
@@ -5,13 +5,13 @@ import de.htwg.se.model._
 import scala.io.StdIn.readLine
 import de.htwg.se.util._
 
-class TUI(controller: Controller) extends Observer:
+/*class TUI(controller: Controller) extends Observer:
     controller.add(this)
     def run = 
         println(controller.toString)
         getInputAndPrintLoop()
 
-    override def update = ???
+    override def update = println(controller.field.toString)
 
     def getInputAndPrintLoop(): Unit =
     println("Enter your move (<Stone><x><y>, eg. x02, q to quit):")
@@ -31,4 +31,40 @@ class TUI(controller: Controller) extends Observer:
         controller.put(point, x, y)
         println(controller.toString)
         getInputAndPrintLoop()
+      }*/
+
+class TUI(controller: Controller) extends Observer {
+  controller.add(this)
+
+  def run = {
+    println(controller.toString)
+    var input: String = ""
+    while (input != "q") {
+      getInputAndPrintLoop()
+      input = readLine
+    }
+  }
+
+  override def update = println(controller.field.toString)
+
+  def getInputAndPrintLoop(): Unit = {
+    println("Enter your move (<Stone><x><y>, eg. x02, q to quit):")
+    val input = readLine
+    input match {
+      case _ => {
+        val chars = input.toCharArray
+        val point = chars(0) match {
+          case 'W' => Point.w
+          case 'B' => Point.b
+          case 'G' => Point.g
+          case 'R' => Point.r
+          case _  => Point.e
+        }
+        val x = chars(1).toString.toInt
+        val y = chars(2).toString.toInt
+        controller.put(point, x, y)
+        println(controller.toString)
       }
+    }
+  }
+}
