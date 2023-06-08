5889c15262fed7a38a235b734f1cb56b6fde3a26
DanielPaustian <paustian.daniel@gmail.com>
2023-06-08 18:20:50 +0200

    added few test back for testing

--- src/test/scala/de/htwg/se/controler/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/controler/ControllerSpec.scala
@@ -3,4 +3,18 @@ package de.htwg.se.controler
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 
+class ControllerSpec extends AnyWordSpec {
+    "A Field in Mastermind game" when {
+        "undo a point" should {
+            val emptyField = new de.htwg.se.model.Field(4, 6, de.htwg.se.model.PointFactory.createPoint(" "))
+            val controller = Controller(emptyField, null)
+            val field = new de.htwg.se.model.Field(4, 6, de.htwg.se.model.PointFactory.createPoint(" "))
+            field.put(de.htwg.se.model.PointFactory.createPoint("R"), 0, 0)
+            controller.undoLastMove()
+            "have the last game state" in {
+                field should be(emptyField)
+            }
+        }
+    }
+}
 
