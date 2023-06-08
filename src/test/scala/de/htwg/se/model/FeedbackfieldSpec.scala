4d4a93c7617b540808aa24bb801550df731b6ba3
DanielPaustian <paustian.daniel@gmail.com>
2023-06-08 18:54:20 +0200

    deleted test

--- src/test/scala/de/htwg/se/model/FeedbackfieldSpec.scala
+++ src/test/scala/de/htwg/se/model/FeedbackfieldSpec.scala
@@ -14,13 +14,6 @@ class FeedbackFieldSpec extends AnyWordSpec {
         }
       }
     }
-    "filled" should {
-      val field = new Field(4, 6, PointFactory.createPoint(" "))
-      field.put(PointFactory.createPoint("R"), 0, 0)
-      "calculate the correct feedback" in {
-        de.htwg.se.model.FeedbackField.calculateFeedback(field, 0, 0) should be(Feedback.PositionCorrect)
-      }
-    }
   }
 } 
 
