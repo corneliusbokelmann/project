ee4865b97249b090582c6226183bc32fbf316cca
DanielPaustian <paustian.daniel@gmail.com>
2023-06-09 00:19:15 +0200

    Added tests for RemoveCommand

---
+++ src/test/scala/de/htwg/se/controler/RemoveCommandSpec.scala
@@ -0,0 +1,22 @@
+
+
+package de.htwg.se.controler
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
+import de.htwg.se.model.{Field,PointFactory}
+import scala.util.{Success,Try,Failure}
+
+class RemoveCommandSpec extends AnyWordSpec {
+    "RemoveCommand " should {
+        val field = new Field(2, 3, PointFactory.createPoint("R"))
+        val receiver = new Receiver(field)
+        val removeCommand = new RemoveCommand(receiver, 0, 0)
+        "execute and undo" in {
+            removeCommand.execute() should be(Try(receiver.remove(0, 0)).map(_ => ()))
+            removeCommand.undo() should be(Success(()))
+        }
+    }
+}
+
+
