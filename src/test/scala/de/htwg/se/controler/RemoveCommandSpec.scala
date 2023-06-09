4668c5e1e5c37e526b21849bfbae486926a3a858
DanielPaustian <paustian.daniel@gmail.com>
2023-06-09 08:33:02 +0200

    Added more tests for RemoveCommand

--- src/test/scala/de/htwg/se/controler/RemoveCommandSpec.scala
+++ src/test/scala/de/htwg/se/controler/RemoveCommandSpec.scala
@@ -8,13 +8,23 @@ import de.htwg.se.model.{Field,PointFactory}
 import scala.util.{Success,Try,Failure}
 
 class RemoveCommandSpec extends AnyWordSpec {
-    "RemoveCommand " should {
-        val field = new Field(2, 3, PointFactory.createPoint("R"))
-        val receiver = new Receiver(field)
-        val removeCommand = new RemoveCommand(receiver, 0, 0)
-        "execute and undo" in {
-            removeCommand.execute() should be(Try(receiver.remove(0, 0)).map(_ => ()))
-            removeCommand.undo() should be(Success(()))
+    "RemoveCommand " when {
+        "filled" should{
+            val field = new Field(2, 3, PointFactory.createPoint("R"))
+            val receiver = new Receiver(field)
+            val removeCommand = new RemoveCommand(receiver, 0, 0)
+            "execute and undo" in {
+                removeCommand.execute() should be(Try(receiver.remove(0, 0)).map(_ => ()))
+                removeCommand.undo() should be(Success(()))
+            }
+        }
+        "empty" should{
+            val field = new Field(2, 3, PointFactory.createPoint(""))
+            val receiver = new Receiver(field)
+            val removeCommand = new RemoveCommand(receiver, 0, 0)
+            "execute and undo" in {
+                removeCommand.undo() should be(Try(receiver.add(PointFactory.createPoint("G"), 0, 0)))
+            }
         }
     }
 }
