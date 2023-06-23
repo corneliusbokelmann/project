c302ce618b35980a29ca0a5345bc593e69a25ab3
corneliusbokelmann <bokelmanncornelius@web.de>
2023-06-23 08:48:05 +0200

    fixed gui

--- src/main/scala/de/htwg/se/Main.scala
+++ src/main/scala/de/htwg/se/Main.scala
@@ -4,29 +4,22 @@ import de.htwg.se.model.{Field, FeedbackField, Matrix, Point, PointFactory}
 import de.htwg.se.aview.{TUI, StandardInput, GUI}
 import de.htwg.se.controler.{Controller, AddCommand, RemoveCommand}
 
+
 object Mastermind {
   def main(args: Array[String]): Unit = {
     println("Welcome to Mastermind!")
+
     val field = Field(Matrix(Vector.fill(10, 4)(Some(PointFactory.createPoint(" ")))))
     val feedbackField = FeedbackField(guesslength = 10)
-    val controller = Controller(field, feedbackField) // Gui not passed
+    val controller = Controller(field, feedbackField)
 
-    val tui = new TUI(controller, new StandardInput())
+    val tui = TUI(controller, new StandardInput())
+    val gui = GUI(controller)
 
-    val threadTui = new Thread {
-      override def run(): Unit = {
-        tui.run()
-      }
-    }
+    // Start the TUI
+    tui.run()
 
-    val threadGui = new Thread {
-      override def run(): Unit = {
-        val gui = new GUI(controller)
-        gui.visible = true
-      }
-    }
-
-    threadTui.start()
-    threadGui.start()
+    // Show the GUI
+    gui.visible = true
   }
 }
--- src/main/scala/de/htwg/se/aview/GUI.scala
+++ src/main/scala/de/htwg/se/aview/GUI.scala
@@ -3,27 +3,61 @@ package de.htwg.se.aview
 import scala.swing._
 import de.htwg.se.controler.Controller
 import de.htwg.se.util.Observer
+import de.htwg.se.model.Point
+import java.awt.Color
+import javax.swing.BorderFactory
+import de.htwg.se.model.WhitePoint
+import de.htwg.se.model.BlackPoint
+import de.htwg.se.model.GreenPoint
+import de.htwg.se.model.RedPoint
 
 class GUI(controller: Controller) extends MainFrame with Observer {
   title = "Mastermind"
   preferredSize = new Dimension(800, 600)
 
-  // Create a label for each position in the game board
-  val gameBoard = new GridPanel(controller.field.matrix.guesslength, controller.field.matrix.pointslength) {
+  // Create a panel for the playing field
+  val gameBoardPanel = new GridPanel(controller.field.matrix.guesslength, controller.field.matrix.pointslength) {
     for {
       x <- 0 until controller.field.matrix.guesslength
       y <- 0 until controller.field.matrix.pointslength
     } {
-      contents += new Label {
+      val label = new Label {
         text = controller.field.matrix.cell(x, y).toString
         font = new Font("Arial", 1, 24)
+        opaque = true
+        background = getColorForPoint(controller.field.matrix.cell(x, y))
+        border = BorderFactory.createLineBorder(Color.black)
+        preferredSize = new Dimension(60, 60)
+        horizontalAlignment = Alignment.Center
+        verticalAlignment = Alignment.Center
       }
+      contents += label
     }
   }
 
-  // Combine the labels into one layout
-  contents = new GridPanel(1, 1) {
-    contents += gameBoard
+  // Create a panel for the feedback field
+  val feedbackFieldPanel = new GridPanel(controller.feedbackField.guesslength, 1) {
+    for {
+      x <- 0 until controller.feedbackField.guesslength
+    } {
+      val label = new Label {
+        text = controller.feedbackField.getFeedbackMatrix.cell(x, 0).toString
+        font = new Font("Arial", 1, 24)
+        opaque = true
+        background = Color.lightGray
+        border = BorderFactory.createLineBorder(Color.black)
+        preferredSize = new Dimension(60, 60)
+        horizontalAlignment = Alignment.Center
+        verticalAlignment = Alignment.Center
+      }
+      contents += label
+    }
+  }
+
+  // Combine the playing field and feedback field panels into one layout
+  contents = new BorderPanel {
+    layout(gameBoardPanel) = BorderPanel.Position.Center
+    layout(feedbackFieldPanel) = BorderPanel.Position.East
   }
 
   // Update the GUI when the game state changes
@@ -34,8 +68,27 @@ class GUI(controller: Controller) extends MainFrame with Observer {
       x <- 0 until controller.field.matrix.guesslength
       y <- 0 until controller.field.matrix.pointslength
     } {
-      val label = gameBoard.contents(y * controller.field.matrix.guesslength + x).asInstanceOf[Label]
+      val label = gameBoardPanel.contents(y * controller.field.matrix.guesslength + x).asInstanceOf[Label]
       label.text = controller.field.matrix.cell(x, y).toString
+      label.background = getColorForPoint(controller.field.matrix.cell(x, y))
+    }
+
+    // Update the feedback field labels
+    for {
+      x <- 0 until controller.feedbackField.guesslength
+      label = feedbackFieldPanel.contents(x).asInstanceOf[Label]
+    } {
+      label.text = controller.feedbackField.getFeedbackMatrix.cell(x, 0).toString
+    }
+  }
+
+  private def getColorForPoint(point: Option[Point]): Color = {
+    point match {
+      case Some(WhitePoint) => Color.white
+      case Some(BlackPoint) => Color.black
+      case Some(GreenPoint) => Color.green
+      case Some(RedPoint)   => Color.red
+      case _                => Color.lightGray
     }
   }
 
